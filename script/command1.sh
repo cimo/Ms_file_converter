e84ed358250ecb286d84b90718e2d0c7857e0b7f
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-01-20 19:06:20 +0900

    update

--- script/command1.sh
+++ script/command1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-"/home/squashfs-root/AppRun" --headless --convert-to "${1}" "${2}" --outdir "${3}${4}/" > /dev/null 2>&1 &
+"/home/squashfs-root/AppRun" --headless --convert-to "${1}" "${2}" --outdir "${3}${4}/" >> ${PATH_ROOT}${MS_FC_PATH_LOG}libreoffice.log 2>&1 &
