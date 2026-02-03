3390513e62fe92f0d52fb223dd6f40cf57c6fd3c
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-02-03 10:02:00 +0900

    update

--- script/libreoffice.sh
+++ script/libreoffice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-XDG_RUNTIME_DIR="/mnt/wslg/runtime-dir" "/home/squashfs-root/AppRun" --nologo >> "${PATH_ROOT}${MS_FC_PATH_LOG}libreoffice.log" 2>&1 &
+"/home/squashfs-root/AppRun" --nologo >> "${PATH_ROOT}${MS_FC_PATH_LOG}libreoffice.log" 2>&1 &
